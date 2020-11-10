79bbd0828e6dd200103b28ba0a7cf1425194bdde
amansil3 <amansil3@rosario.gov.ar>
2020-11-10 14:08:40 -0300

    Trabajando Index con TKinter

--- index.py
+++ index.py
@@ -10,24 +10,24 @@ class Product:
         self.wind.title('Product Application')
 
         #Frame
-        frame = LabelFrame(self.wind, text = 'Registrar un nuevo producto')
+        frame = LabelFrame(self.wind, text = 'Registrar un nuevo cliente')
         frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
 
-        #name Input
+        # Input Nombre
         Label(frame, text = 'Nombre: ').grid(row = 1, column = 0)
         Entry(frame)
         self.name = Entry(frame)
         self.name.focus()
         self.name.grid(row = 1, column = 1)
 
-        #precio Input
-        Label(frame, text = 'Precio: ').grid(row = 2, column = 0)
+        # Input Apellido
+        Label(frame, text = 'Apellido: ').grid(row = 2, column = 0)
         Entry(frame)
         self.price = Entry(frame)
         self.price.grid(row = 2, column = 1)
 
-        #boton producto 
-        ttk.Button(frame, text = "Guardar Producto").grid(row = 3, columnspan = 2, sticky = W + E)
+        # Button Guardar
+        ttk.Button(frame, text = "Guardar Cliente").grid(row = 3, columnspan = 2, sticky = W + E)
 
         # Tabla
         self.tree = ttk.Treeview(height = 10, columns = 2)
